f2776476c91775e1611f790df5703a4425e681ab
polo dev <polodev10@gmail.com>
2018-01-29 15:59:36 +0600

    added jquery function

--- script.js
+++ script.js
@@ -10,15 +10,35 @@ h1.on('click', function () {
   alert('hello world');
 });
 
-function hello() {
-  console.log('hello world');
-}
+// function hello() {
+//   console.log('hello world');
+// }
 
-var parvez = hello;
+// var parvez = hello;
 
-hello();
-parvez();
+// hello();
+// parvez();
+// single line comment 
+/*
+css and js comments
+*/
+//hide function 
 
+$('button.hide').on('click', function () {
+  // $('p.first_paragraph').hide(300);
+  // $('p.first_paragraph').fadeOut(900);
+  $('p.first_paragraph').slideUp(900);
+})
+$("button.show").on('click', function() {
+  // $('p.first_paragraph').show(300);
+  // $('p.first_paragraph').fadeIn(900);
+  $('p.first_paragraph').slideDown(900);
+})
+$('button.toggle').on("click", function () {
+  // $('p.first_paragraph').toggle(900);
+  // $('p.first_paragraph').slideToggle(900);
+  $('p.first_paragraph').fadeToggle(900);
+})
 
 
 
